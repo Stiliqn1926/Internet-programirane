d941b6eaa960bc1a9da6daa4b9dc044ba9af77a4
Stiliqn1926 <stiliqn1502@gmail.com>
2025-12-12 22:11:29 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-import reactLogo from './react.svg';
-import Task2 from './Task2';
+import Button from "./Button";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Props Overview – `children` demo</h1>
-
-      <Task2 firstName="Ivan" lastName="Danovski" title="Teacher" image={reactLogo}>
-        This is the "children" prop
-      </Task2>
-    </div>
+    <ul>
+      <Button>Button 1</Button>
+      <Button>Button 2</Button>
+    </ul>
   );
 }
